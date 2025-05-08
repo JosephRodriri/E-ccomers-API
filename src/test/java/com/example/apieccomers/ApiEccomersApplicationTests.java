373bdb288791094939b91d048ce99f56ec78442f
Joseph Rodriguez <josephrodriri@gmail.com>
2025-05-08 00:09:20 -0500

    Creacion estrucutra proyecto y clases model

---
+++ src/test/java/com/example/apieccomers/ApiEccomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apieccomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEccomersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
